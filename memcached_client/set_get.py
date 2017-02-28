824d0b275a13676fd660702ca674ed1ccda30d90
Yuxia <rainycheng1988@163.com>
2017-02-28 15:45:24 +1100

    add encrypt

--- memcached_client/set_get.py
+++ memcached_client/set_get.py
@@ -9,6 +9,17 @@ i=i+1
 command = './my_client ' + str(i) + ' ' + str(value-100)
 os.system(command)
 
+value=1024
+for i in range (1,11):
+    command = './my_client_enc ' + str(i) + ' ' + str(value)
+    value=value*2
+    print command
+    os.system(command)
+i=i+1
+command = './my_client_enc ' + str(i) + ' ' + str(value-100)
+os.system(command)
+
+
 for i in range (1,12):
     command = './my_client_get ' + str(i)
     print command
